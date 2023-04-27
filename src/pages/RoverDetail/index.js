8967d3677b60870464cfd0da822129b66db8e8cf
Christopher Burnette <chrisburnette083@gmail.com>
2023-04-27 17:06:23 +0500

    implemented search by date filter for rover detail

--- src/pages/RoverDetail/index.js
+++ src/pages/RoverDetail/index.js
@@ -2,19 +2,23 @@ import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
 import CircularProgress from '@mui/material/CircularProgress';
 import Api from "./../../helper/api";
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import moment from 'moment';
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+
 const RoverDetail = () => {
     const api = new Api();
     const [roverDetail, setRoverDetail] = useState();
     const [loading, setLoading] = useState(false);
     const [date, setDate] = useState(moment().format('YYYY-MM-DD'))
     const param = useParams();
-    const fetchUser = () => {
+    const fetchUser = (date) => {
         setLoading(true);
         api
-            .getRoverDetail(param.name, '2021-12-3')
+            .getRoverDetail(param.name, date)
             .then((response) => {
                 setLoading(false);
                 setRoverDetail(response.data.photos);
@@ -29,9 +33,13 @@ const RoverDetail = () => {
         // fetchUser();
         setRoverDetail(require('./../../rover-detail.json').photos)
     }, []);
+
+    useEffect(() => {
+        fetchUser(date)
+    }, [date])
     return (
-        <Box
-            sx={{
+        <Box>
+            <Box sx={{
                 display: 'flex',
                 flexWrap: 'wrap',
                 justifyContent: 'center',
@@ -39,34 +47,52 @@ const RoverDetail = () => {
                     m: 2,
                     p: 2,
                     width: 280,
-                    height: 220,
                 }
-            }}
-        >
-            {loading ?
-                <CircularProgress />
-                :
-                roverDetail?.length > 0 && roverDetail.map((item, i) => (<Paper
-                    key={i}
-                    sx={{
-                        cursor: 'pointer'
-                    }}
-                    elevation={3}>
-                    <div className='CardContent'>
-                        <Box
-                            component="img"
-                            sx={{
-                                height: '100%',
-                                width: '100%',
-                            }}
-                            alt="The house from the offer."
-                            src={item.img_src}
-                        />
-                    </div>
-                </Paper>))
-            }
+            }}>
+                <LocalizationProvider dateAdapter={AdapterDateFns}>
+                    <DatePicker onChange={(e) => setDate(moment(e).format('YYYY-MM-DD'))} />
+                </LocalizationProvider>
+            </Box>
+            <Box
+                sx={{
+                    display: 'flex',
+                    flexWrap: 'wrap',
+                    justifyContent: 'center',
+                    '& > :not(style)': {
+                        m: 2,
+                        p: 2,
+                        width: 280,
+                        height: 220,
+                    }
+                }}
+            >
 
+                {loading ?
+                    <CircularProgress />
+                    :
+                    roverDetail?.length > 0 && roverDetail.map((item, i) => (<Paper
+                        key={i}
+                        sx={{
+                            cursor: 'pointer'
+                        }}
+                        elevation={3}>
+                        <div className='CardContent'>
+                            <Box
+                                component="img"
+                                sx={{
+                                    height: '100%',
+                                    width: '100%',
+                                }}
+                                alt="The house from the offer."
+                                src={item.img_src}
+                            />
+                        </div>
+                    </Paper>))
+                }
+
+            </Box>
         </Box>
+
     )
 }
 export default RoverDetail;
